e97baa729ca09005e3722c8cd144293a2d74efef
Kodah <149893697+Kodah@users.noreply.github.com>
2026-08-29 23:20:00 +0300

    Add Student class with default constructor

---
+++ DataType/Construct default.cs
@@ -0,0 +1,12 @@
+class Student
+{
+    public Student()
+    {
+        Console.WriteLine("Construct Created!");
+    }
+
+    static void Main()
+    {
+        Student St = new Student();
+    }
+}
