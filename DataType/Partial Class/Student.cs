e398251229f5051288daf6ae2b50a39d76b07b85
Kodah <149893697+Kodah@users.noreply.github.com>
2026-08-30 20:31:26 +0300

    Create partial class Student with properties and method

---
+++ DataType/Partial Class/Student.cs
@@ -0,0 +1,12 @@
+using System;
+
+partial class Student
+{
+    public string NameSt;
+    public int AgeSt;
+
+    public void printName()
+    {
+        Console.WriteLine("Name: " + NameSt);
+    }
+}
