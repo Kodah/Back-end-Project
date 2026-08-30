bbf8e4a6dbc9ffb4c0dd10afccff41bcb5c7c589
Kodah <149893697+Kodah@users.noreply.github.com>
2026-08-30 20:32:01 +0300

    Implement Student methods for printing age and greeting

---
+++ DataType/Partial Class/StudentMethods.cs
@@ -0,0 +1,15 @@
+using System;
+
+partial class Student
+{
+    public void printAge()
+    {
+        Console.WriteLine("Age: " + AgeSt);
+    }
+
+    public void SayHello()
+    {
+        Console.WriteLine("Hello my name's: " + NameSt);
+    }
+
+}
