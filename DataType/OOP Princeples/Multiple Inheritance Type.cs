981993dbc33ef9bfa1f84404a0c7e94f9739d71d
Kodah <149893697+Kodah@users.noreply.github.com>
2026-09-05 01:15:14 +0300

    Implement multiple inheritance using interfaces in C#

---
+++ DataType/OOP Princeples/Multiple Inheritance Type.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Program
+{
+    
+   interface Ifather
+    {
+        void Money();
+    }
+    
+    interface Imother
+    {
+        void Hair();
+    }
+
+    class SON
+    {
+        public void Money()
+        {
+            Console.WriteLine("I have Money from my father.");
+        }
+
+        public void Hair()
+        {
+            Console.WriteLine("My hair like the type of My mother's hair.");
+        }
+    }
+    static void Main()
+    {
+        SON son = new SON();
+
+        son.Money();
+        son.Hair();
+    }
+ 
+}
